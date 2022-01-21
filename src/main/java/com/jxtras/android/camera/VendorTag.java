4c696f0bb94ee472b0416ba14eaacaab50facf24
Tony Guo <tonykwok@users.noreply.github.com>
2022-01-21 11:26:17 +0800

    Create VendorTag.java

---
+++ src/main/java/com/jxtras/android/camera/VendorTag.java
@@ -0,0 +1,45 @@
+package com.jxtras.android.camera;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public abstract class VendorTag<K> {
+    private final static AtomicInteger sIdGenerator = new AtomicInteger(0);
+
+    private final AtomicReference<K> mCachedKey = new AtomicReference<>(null);
+    private final int id;
+
+    public VendorTag() {
+        id = generateId();
+    }
+
+    private static int generateId() {
+        return sIdGenerator.getAndIncrement();
+    }
+
+    public final K getKey() {
+        K key = mCachedKey.get();
+        if (key == null) {
+            synchronized (mCachedKey) {
+                key = mCachedKey.get();
+                if (key == null) {
+                    mCachedKey.set(createKey());
+                }
+            }
+        }
+        return mCachedKey.get();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public final boolean equals(Object o) {
+        return o instanceof VendorTag && ((VendorTag<K>)o).id == id;
+    }
+
+    @Override
+    public final int hashCode() {
+        return id;
+    }
+
+    protected abstract K createKey();
+}
